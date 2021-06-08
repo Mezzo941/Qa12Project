587ca62839829c55b30a3a3e62688dfb52ab9150
Mezzo941 <mezzo9411@gmail.com>
2021-06-08 21:40:44 +0300

    unit0.1

--- Unit0/src/Unit0Task3.java
+++ Unit0/src/Unit0Task3.java
@@ -1,16 +1,18 @@
 public class Unit0Task3 {
     public static void main(String[] args) {
+        int a=0;
 
-        int a = 1, b = 0, c = 2;
-        if ((a > 0 && b <= 0 && c <= 0) || (a <= 0 && b > 0 && c <= 0) || ((a <= 0 && b <= 0 && c > 0))) {
-            System.out.println("1 положительное число");
-        } else if ((a > 0 && b > 0 && c <= 0) || (a > 0 && b <= 0 && c > 0) || ((a <= 0 && b > 0 && c > 0))) {
-            System.out.println("2 положительных числа");
-        } else if (a > 0 && b > 0 && c > 0) {
-            System.out.println("3 положительных числа");
-        } else {
-            System.out.println("положительных чисел нет");
+        if (a<0){
+            System.out.println(a-2);
         }
 
+        if (a>0){
+            System.out.println(a+1);
+        }
+
+        if (a==0){
+            a=10;
+            System.out.println(a);
+        }
     }
 }
--- Unit0/src/Unit0Task7.java
+++ Unit0/src/Unit0Task7.java
@@ -1,20 +1,18 @@
-import java.util.Scanner;
-
 public class Unit0Task7 {
     public static void main(String[] args) {
+        int prog = (int) (Math.random()*100);
 
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Введите первое значение: ");
-        int a = in.nextInt();
-        System.out.print("Введите второе значение: ");
-        int b = in.nextInt();
-
-        if (a>b){ System.out.println(a + " больше т.к. "+a+" > "+b);
+        if (prog==1 || prog==21 || prog==31 || prog==41 || prog==51 || prog==61 || prog==71 || prog==81  || prog==91){
+            System.out.println(prog+" Программист");
         }
-        if (a<b) {
-            System.out.println(b+ " больше т.к. " + b + " > " + a);
+        if (2<=prog && prog<=4 || 22<=prog && prog<=24 || 32<=prog && prog<=34 || 42<=prog && prog<=44 || 52<=prog &&
+                prog<=54 || 62<=prog && prog<=64 || 72<=prog && prog<=74 || 82<=prog && prog<=84 || 92<=prog && prog<=94)
+            System.out.println(prog+" Программиста");
+        if (prog==0 || 5<=prog && prog<=20 || 25<=prog && prog<=30 || 35<=prog && prog<=40 || 45<=prog && prog<=50 || 55<=prog &&
+                prog<=60 || 65<=prog && prog<=70 || 75<=prog && prog<=80 || 85<=prog && prog<=90 || 95<=prog && prog<=100){
+            System.out.println(prog+" Программистов");
         }
-        if (a==b) { System.out.println("Значения равны т.к. "+a+" = "+b);}
+
     }
 }
+
